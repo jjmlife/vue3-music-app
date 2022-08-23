3efef777740f90ff4dec454f728fe28e9034c27b
jiangjinming <jiangjinming@jm-mac.local>
2022-08-23 22:26:48 +0800

    init

---
+++ src/components/baseui/icon-button/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="icon-button" @click="buttonClick">
+    <div class="icon-bg">
+      <svg-icon :icon-class="icon" class-name="icon"></svg-icon>    
+    </div>
+    <div class="title">{{ title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "IconButton",
+  props:{
+    index: {
+      type: Number,
+      default: 0
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  emits:['item-click'],
+  setup (props, context) {
+    const buttonClick = () => {
+      context.emit('item-click', props.index)
+    }
+    return {
+      buttonClick
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.icon-button {
+  height: 1.5rem;
+  width: 1rem;
+  .icon-bg {
+    height: 1rem;
+    width: 1rem;
+    text-align: center;
+    line-height: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(100, 0, 0, 0.1);
+    border-radius: 50%;
+  }
+  .title {
+    margin-top: .2rem;
+    color: black;
+    font-size: 0.24rem;
+    text-align: center;
+  }
+  .icon {
+    font-size: .6rem;
+  }
+}
+</style>
---
+++ src/components/baseui/svg-icon/sec.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    v-if="isExternal"
+    :style="styleExternalIcon"
+    class="svg-external-icon svg-icon"
+    v-on="$listeners"
+  />
+  <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script lang="ts">
+function isExternal(path: string) {
+  return /^(https?:|mailto:|tel:)/.test(path);
+}
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "SvgIcon",
+  props: {
+    iconClass: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      default: "",
+    },
+  },
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>       
+
+<style scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+
+.svg-external-icon {
+  background-color: currentColor;
+  mask-size: cover !important;
+  display: inline-block;
+}
+</style>
